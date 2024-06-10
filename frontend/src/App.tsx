9f872e7654da66496b649c20325b0cbea675cea4
valedobrandi <valedobrandi@gmail.com>
2024-06-10 17:58:25 -0300

    install daisyUI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
-import './App.css';
+import Drawer from './components/Drawer';
 
 function App() {
   return (
-    <h1>store-manage-app</h1>
+    <>
+      <h1 className="text-center m-10 text-cyan-500">store-manage-app</h1>
+      <Drawer />
+    </>
   );
 }
 
